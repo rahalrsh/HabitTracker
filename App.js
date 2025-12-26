489d58892d328505ef001ed9c9d47524ace06212
Rahal Ranasinghe <rahalrsh@gmail.com>
2025-12-26 14:23:10 -0500

    first screen

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import "./global.css"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
+  const handleAddHabit = () => {
+    console.log("Add Habit");
+  }
+
   return (
     <View style={styles.container}>
-      <Text className="text-xl font-bold text-green-500">Hello world!!</Text>
-      <StatusBar style="auto" />
+      <AntDesign name="plus-circle" size={32} color="#22c55e" />
+      <Text className="text-xl font-bold text-green-500">No habit found</Text>
+      <Text className="text-md text-green-500">Create a new habit to get started</Text>
+      <Pressable 
+        onPress={handleAddHabit}
+        className="bg-green-500 rounded-lg px-6 py-3 mt-4"
+      >
+        <Text className="text-white font-semibold text-base">Get Started</Text>
+      </Pressable>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1c1c1c',
     alignItems: 'center',
     justifyContent: 'center',
   },
